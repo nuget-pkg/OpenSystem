f91d2c0e0d5037fda0eebdcc897e14372c0628bb
javacommons <javacommons@gmail.com>
2026-03-29 05:38:24 +0900

    ⭕️ [️COMMIT] for tag: v2026.0329.0538.11 [DIFF] diff --git a/OpenSystem.Demo/Demo.cs b/OpenSystem.Demo/Demo.cs     -﻿using System;     -using Global;     -using static Global.EasyObjectClassic;     -using NUnit.Framework;     +﻿using Global;     +using static Global.EasyObject;     -   }     +        int a = 111, b = 222;     +        var answer = OpenSystem.Add2(a, b);     +        AssertEqual(expected: 33, actual: answer, hint: new { a, b, answer });     +    } diff --git a/OpenSystem.Demo/OpenSystem.Demo.csproj b/OpenSystem.Demo/OpenSystem.Demo.csproj     -    <PackageReference Include="coverlet.collector" Version="8.0.1">     -      <PrivateAssets>all</PrivateAssets>     -      <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>     -    </PackageReference>     -    <PackageReference Include="EasyObjectClassic" Version="2026.211.39.11" />     -    <PackageReference Include="NUnit" Version="4.5.1" />     -    <PackageReference Include="NUnit3TestAdapter" Version="6.2.0" />     -    <PackageReference Include="NUnit.Analyzers" Version="4.12.0">     -      <PrivateAssets>all</PrivateAssets>     -      <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>     -    </PackageReference>     +    <PackageReference Include="EasyObject" Version="*" /> diff --git a/OpenSystem/OpenSystem.csproj b/OpenSystem/OpenSystem.csproj     -    <Version>2026.0328.2036.51</Version>     +    <Version>2026.0329.0538.11</Version>     -    <PackageReference Include="UniversalEncoding" Version="2026.328.2018.21" />     +    <PackageReference Include="UniversalEncoding" Version="2026.329.520.34" /> diff --git a/version.txt b/version.txt     -2026.0328.2036.51     +2026.0329.0538.11 [/DIFF]

--- OpenSystem.Demo/Demo.cs
+++ OpenSystem.Demo/Demo.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Global;
-using static Global.EasyObjectClassic;
-using NUnit.Framework;
+﻿using Global;
+using static Global.EasyObject;
 
 // ReSharper disable once CheckNamespace
 namespace Demo;
@@ -14,5 +12,8 @@ static class Program
     {
         Echo(new { args = args });
         Echo(OpenSystem.Add2(11, 22));
-   }
+        int a = 111, b = 222;
+        var answer = OpenSystem.Add2(a, b);
+        AssertEqual(expected: 33, actual: answer, hint: new { a, b, answer });
+    }
 }
