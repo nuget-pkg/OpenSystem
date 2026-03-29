9c2ed83796e93fc63b5f12243cd91491ef944b42
javacommons <javacommons@gmail.com>
2026-03-30 04:33:19 +0900

    .

---
+++ OpenSystem.Misc/EasyExtensions.cs
@@ -0,0 +1,62 @@
+﻿namespace Global;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+public static class EasyExtensions
+{
+    public static dynamic? FromJson(this string x)
+    {
+        return EasyToolbox.FromJson(x);
+    }
+
+    public static T? FromJson<T>(this string x, T? fallback = default(T))
+    {
+        return EasyToolbox.FromJson<T>(x, fallback);
+    }
+
+    public static XmlDocument ToXmlDocument(this XDocument xDocument)
+    {
+        var xmlDocument = new XmlDocument();
+        using (var xmlReader = xDocument.CreateReader())
+        {
+            xmlDocument.Load(xmlReader);
+        }
+
+        return xmlDocument;
+    }
+
+    public static XDocument ToXDocument(this XmlDocument xmlDocument)
+    {
+        using (var nodeReader = new XmlNodeReader(xmlDocument))
+        {
+            nodeReader.MoveToContent();
+            return XDocument.Load(nodeReader);
+        }
+    }
+
+    public static string ToStringWithDeclaration(this XDocument doc)
+    {
+        if (doc == null)
+        {
+            throw new ArgumentNullException("doc");
+        }
+
+        using (TextWriter writer = new Utf8StringWriter())
+        {
+            doc.Save(writer);
+            return writer.ToString()!;
+        }
+    }
+
+    internal class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+    }
+}
---
+++ OpenSystem.Misc/OpenSystem.Misc01.cs
@@ -0,0 +1,78 @@
+﻿// ReSharper disable RedundantUsingDirective
+
+//using Spectre.Console.Json.Syntax;
+
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace Misc;
+
+using Global;
+using static Global.EasyObject;
+using static Global.EasySystem;
+
+public class Misc01
+{
+    public static void Main(string[] args)
+    {
+        SetupConsoleEncoding();
+        ShowLineNumbers = false;
+        ShowDetail = true;
+        UseAnsiConsole = true;
+        DebugOutput = true;
+        Log("⭕️ハロー©⭕️");
+        EasySystem.RunToConsole("bash", ["-c", "ls -ltr"]);
+        JArray? newton = EasyToolbox.FromJson("[11,null,33.15,[44,55], {'a': 123}]");
+        Log(newton, title: "newton");
+        if (newton != null)
+        {
+            var o = newton.ToObject<object[]>();
+            Log(o);
+        }
+
+        var o2 = EasyToolbox.FromObject(newton);
+        Log(o2, title: "o2");
+        var eo = ParseObject(newton);
+        Log(eo, title: "eo");
+    }
+
+    public static EasyObject ParseObject(dynamic? x)
+    {
+        if (x == null) return Null;
+        if (x is JArray jarray)
+        {
+            //return FromObject(array.ToObject<object[]>());
+            List<object> array = jarray.ToObject<List<object>>()!;
+            var result = NewArray();
+            foreach (var item in array)
+            {
+                result.Add(ParseObject(item));
+            }
+
+            return result;
+        }
+        else if (x is JObject jobject)
+        {
+            Dictionary<string, object> dict = jobject.ToObject<Dictionary<string, object>>()!;
+            var result = NewObject();
+            var keys = dict.Keys;
+            //return FromObject(keys);
+            foreach (var key in keys)
+            {
+                result.Add(key, ParseObject(dict[key]));
+            }
+
+            return result;
+        }
+        else
+        {
+            string typeName = FullName(x);
+            if (typeName.StartsWith("System."))
+            {
+                return FromObject(x);
+            }
+
+            return FullName(x);
+        }
+    }
+}
