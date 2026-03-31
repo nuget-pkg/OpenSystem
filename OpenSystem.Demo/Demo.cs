869fbdcdbe79671de50ebb57d777b75daa39d6c7
javacommons <javacommons@gmail.com>
2026-03-31 19:10:56 +0900

    .

--- OpenSystem.Demo/Demo.cs
+++ OpenSystem.Demo/Demo.cs
@@ -1,8 +1,15 @@
 ﻿using Global;
 using static Global.EasyObject;
 
-Echo(new { args = args });
-Echo(OpenSystem.Add2(11, 22));
-int a = 111, b = 222;
-var answer = OpenSystem.Add2(a, b);
-AssertEqual(expected: 33, actual: answer, hint: new { a, b, answer });
+try
+{
+    Echo(new { args = args });
+    Echo(OpenSystem.Add2(11, 22));
+    int a = 111, b = 222;
+    var answer = OpenSystem.Add2(a, b);
+    AssertUnified(expected: 33, actual: answer, hint: new { a, b, answer });
+}
+catch(System.Exception ex)
+{
+    Abort(ex);
+}
