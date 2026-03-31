adca0f536b52d88f5231457079d51a857957c646
javacommons <javacommons@gmail.com>
2026-03-31 23:11:09 +0900

    ⭕️ [️COMMIT] for tag: v2026.0331.2311.00 [DIFF] diff --git a/OpenSystem.Misc/OpenSystem.Misc.csproj b/OpenSystem.Misc/OpenSystem.Misc.csproj     -        <PackageReference Include="EasyObjectClassic" Version="*" />     +        <PackageReference Include="EasyObjectClassic" Version="2026.331.2223.35" /> diff --git a/OpenSystem.Misc/OpenSystem.Misc01.cs b/OpenSystem.Misc/OpenSystem.Misc01.cs     -using static Global.EasySystem;     +//using static Global.EasySystem;     -        //Setup     +        SetupConsoleEncoding();     -        //Log("⭕️ハロー©⭕️");     -        Global.EasySystem.RunToConsole(Encoding.UTF8, "bash", ["-c", "ls -ltr"]);     +        Log("⭕️ハロー©⭕️⁅記号⁆◉▶▸⸝↪️ ↩️ ℴ𝓬➺➢ᰔ  ヾ➠✅🈂️❓❗＼／：＊“≪≫￤；‘｀＃％＄＆＾～￤﴾﴿⁅⁆【】≪≫＋ー＊＝⚽ 𝑪𝒉𝒆𝒄𝒌 🌐🪩");     +        EasySystem.RunToConsole(Encoding.UTF8, "bash", ["-c", "ls -ltr"]); diff --git a/OpenSystem.Test/OpenSystem.Test.csproj b/OpenSystem.Test/OpenSystem.Test.csproj     -    <PackageReference Include="EasyObjectClassic" Version="2026.330.525.17" />     +    <PackageReference Include="EasyObjectClassic" Version="2026.331.2223.35" /> diff --git a/OpenSystem/OpenSystem.csproj b/OpenSystem/OpenSystem.csproj     -        <Version>2026.0331.2306.49</Version>     +        <Version>2026.0331.2311.00</Version>     -        <Reference Include="Microsoft.CSharp"/>     -        <Reference Include="System"/>     -        <Reference Include="System.Core"/>     -        <Reference Include="System.Data"/>     -        <Reference Include="System.Data.DataSetExtensions"/>     -        <Reference Include="System.IO.Compression"/>     -        <Reference Include="System.Linq"/>     -        <Reference Include="System.Net.Http"/>     -        <Reference Include="System.Web"/>     -        <Reference Include="System.Xml"/>     -        <Reference Include="System.Xml.Linq"/>     +        <Reference Include="Microsoft.CSharp" />     +        <Reference Include="System" />     +        <Reference Include="System.Core" />     +        <Reference Include="System.Data" />     +        <Reference Include="System.Data.DataSetExtensions" />     +        <Reference Include="System.IO.Compression" />     +        <Reference Include="System.Linq" />     +        <Reference Include="System.Net.Http" />     +        <Reference Include="System.Web" />     +        <Reference Include="System.Xml" />     +        <Reference Include="System.Xml.Linq" />     -        <None Include=".\README.md" Pack="true" PackagePath=""/>     +        <None Include=".\README.md" Pack="true" PackagePath="" />     -        <PackageReference Include="Microsoft.CSharp" Version="4.7.0"/>     -        <PackageReference Include="System.Dynamic.Runtime" Version="4.3.0"/>     +        <PackageReference Include="Microsoft.CSharp" Version="4.7.0" />     +        <PackageReference Include="System.Dynamic.Runtime" Version="4.3.0" />     -        <PackageReference Include="EasyObjectClassic" Version="2026.330.525.17"/>     -        <PackageReference Include="System.Text.Encoding.CodePages" Version="10.0.5"/>     -        <PackageReference Include="UniversalEncoding" Version="2026.329.520.34"/>     +        <PackageReference Include="EasyObjectClassic" Version="2026.331.2223.35" />     +        <PackageReference Include="System.Text.Encoding.CodePages" Version="10.0.5" />     +        <PackageReference Include="UniversalEncoding" Version="2026.329.520.34" /> diff --git a/version.txt b/version.txt     -2026.0331.2306.49     +2026.0331.2311.00 [/DIFF]

--- OpenSystem.Misc/OpenSystem.Misc01.cs
+++ OpenSystem.Misc/OpenSystem.Misc01.cs
@@ -4,14 +4,14 @@ using System.Text;
 //using Newtonsoft.Json.Linq;
 namespace Misc;
 using Global;
-using static Global.EasySystem;
+//using static Global.EasySystem;
 using static Global.EasyObjectClassic;
 public class Misc01 {
     public static void Main(string[] args) {
-        //Setup
+        SetupConsoleEncoding();
         DebugOutput = true;
-        //Log("⭕️ハロー©⭕️");
-        Global.EasySystem.RunToConsole(Encoding.UTF8, "bash", ["-c", "ls -ltr"]);
+        Log("⭕️ハロー©⭕️⁅記号⁆◉▶▸⸝↪️ ↩️ ℴ𝓬➺➢ᰔ  ヾ➠✅🈂️❓❗＼／：＊“≪≫￤；‘｀＃％＄＆＾～￤﴾﴿⁅⁆【】≪≫＋ー＊＝⚽ 𝑪𝒉𝒆𝒄𝒌 🌐🪩");
+        EasySystem.RunToConsole(Encoding.UTF8, "bash", ["-c", "ls -ltr"]);
         var gvim = EasySystem.FindExeRecursive(@"C:\Program Files\Vim", "gvim.exe");
         Log(gvim, title: "gvim");
     }
