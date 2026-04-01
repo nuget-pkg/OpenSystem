a0a4153249781ccc8bcffac499c7c47a8cf14d02
javacommons <javacommons@gmail.com>
2026-04-02 06:58:06 +0900

    .

--- OpenSystem.Demo/Demo.cs
+++ OpenSystem.Demo/Demo.cs
@@ -1,22 +1,19 @@
 using Global;
 using static Global.EasyObject;
-
-try
-{
+try {
     Echo(new { args = args });
     //Echo(OpenSystem.Add2(11, 22));
     int a = 111, b = 222;
     var answer = a + b; //OpenSystem.Add2(a, b);
     AssertEquivalent(expected: 33, actual: answer, hint: new { a, b, answer });
 }
-catch(System.Exception ex)
-{
+catch (System.Exception ex) {
     //Abort(ex);
-    DieOnBeliefBetrayed(ex,
+    HandleBetrayedExpectation(ex,
         NewObject(
             "abc", 123,
             "xyz",
             NewArray(11, 22, 33)
-            ),
+        ),
         7777);
 }
